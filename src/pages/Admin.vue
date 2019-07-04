eee7c69f7dcf024fecf174984bcd81126d158ec1
Russell Chapman <chpmnrssll@gmail.com>
2019-07-04 02:45:15 -0600

    Netlify-CMS test

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -9,7 +9,35 @@ export default {
   name: 'Admin',
   // meta: [{ requiresAuth: true }],
   mounted() {
-    window.CMS.init();
+    window.CMS.init({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          repo: 'chpmnrssll/dentalcarearvada',
+          squash_merges: true,
+        },
+        load_config_file: false,
+        media_folder: '/uploads',
+        public_folder: '/uploads',
+        collections: [
+          {
+            label: 'Posts',
+            name: 'posts',
+            folder: 'blog',
+            create: true,
+            slug: '{{slug}}',
+            fields: [
+              { label: 'Title', name: 'title', widget: 'string' },
+              { label: 'Excerpt', name: 'excerpt', widget: 'string' },
+              { label: 'Publish Date', name: 'date', widget: 'datetime' },
+              { label: 'Author', name: 'author', widget: 'string' },
+              { label: 'Image', name: 'iamge', widget: 'image' },
+              { label: 'Body', name: 'body', widget: 'markdown' },
+            ],
+          },
+        ],
+      },
+    });
     // console.log(this.$root.$options.CMS);
     // this.$root.$options.CMS.init();
     // CMS.init();
