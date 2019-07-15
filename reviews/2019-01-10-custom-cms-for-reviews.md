b7a816ba8c1998cc83ddfecec8a90d3f370729bb
Russell <chpmnrssll@gmail.com>
2019-07-15 14:06:54 -0600

    Update reviews

--- reviews/2019-01-10-custom-cms-for-reviews.md
+++ reviews/2019-01-10-custom-cms-for-reviews.md
@@ -1,11 +1,9 @@
 ---
 author: Russell
 date: 2019-01-10T08:15:42.896Z
-icon: "assets/uploadedImages/anonymous-avatar-2.png"
+icon: "/src/assets/images/missing-avatar.png"
 location: Simla
 title: Netlify-CMS for Reviews
 ---
-
 # Netlify-CMS for Reviews
-
 This is a test of Netlify-CMS custom post type. Login to Netlify (in the footer of this page) to remove this entry.
--- src/pages/Reviews.vue
+++ src/pages/Reviews.vue
@@ -1,14 +1,23 @@
 <template>
   <DefaultLayout>
     <section class="pageContent">
-      <b-container fluid class="d-flex justify-content-center align-items-center">
-        <b-row class="mb-4">
-          <b-col cols="4" offset="2" v-for="edge in $page.reviews.edges" :key="edge.node.path">
-            <g-link :to="edge.node.path">
-              <g-image :src="edge.node.icon" />
-              <h2 class="mb-4">{{ edge.node.author }}, {{ edge.node.location }}</h2>
-              <h3>{{ edge.node.title }}</h3>
-            </g-link>
+      <b-container fluid>
+        <b-row>
+          <b-col
+            class="d-block mx-auto"
+            xs="12"
+            sm="6"
+            md="6"
+            lg="4"
+            xl="4"
+            v-for="edge in $page.reviews.edges"
+            :key="edge.node.path"
+          >
+            <g-image :src="edge.node.icon" class="d-block mx-auto" />
+            <b-container class="my-4" fluid>
+              <ReviewContent :content="edge.node.content" />
+              <h2 class="text-center">&mdash; {{ edge.node.author }}, {{ edge.node.location }}</h2>
+            </b-container>
           </b-col>
         </b-row>
       </b-container>
@@ -36,7 +45,12 @@ query Review {
 </page-query>
 
 <script>
+import ReviewContent from '../components/ReviewContent.vue';
+
 export default {
+  components: {
+    ReviewContent,
+  },
   metaInfo: {
     title: 'Reviews',
   },
@@ -49,5 +63,9 @@ export default {
   max-width: 1210px;
 }
 .g-image {
+  width: 75%;
+}
+h2 {
+  font-size: 1rem;
 }
 </style>
