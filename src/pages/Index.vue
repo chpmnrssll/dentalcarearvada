53f99695497d46414d5d49298763281ab3474659
Russell <chpmnrssll@gmail.com>
2019-07-24 00:10:01 -0600

    Updated home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -52,14 +52,14 @@
         <template v-slot:body>
           <b-container class="m-lg-auto p-1 p-md-3 px-lg-3">
             <b-row>
-              <b-col md="12" lg="6">
+              <b-col md="12" lg="4" offset="1">
                 <g-image
                   src="~/assets/images/DrW-cropped.jpg"
                   alt="Gregory B. Waters, DDS, PC"
                   class="w-100"
                 />
               </b-col>
-              <b-col class="d-flex flex-column justify-content-center">
+              <b-col lg="6" class="d-flex flex-column justify-content-center">
                 <b-container class="px-0">
                   <h2 class="my-4">Committed To Dental Excellence</h2>
                   <h3 class="mb-4">We create beautiful and brighter smiles</h3>
@@ -71,6 +71,31 @@
                     to offer.
                   </p>
                   <h4 class="my-4">Gregory B. Waters, DDS, PC</h4>
+                </b-container>
+              </b-col>
+            </b-row>
+          </b-container>
+        </template>
+      </HeroSection>
+
+      <HeroSection class="clinically-proven">
+        <template v-slot:body>
+          <b-container class="m-lg-auto p-1 pb-3 my-3 pt-md-0 p-md-3 px-lg-3">
+            <b-row>
+              <b-col lg="6" offset="1" class="d-flex flex-column justify-content-center p-4">
+                <h2><small>Clinically Proven State-of-the-Art Dental Technology</small></h2>
+                <p>
+                  Dr. Waters uses modern, clinically proven methods to offer his patients the best
+                  quality care available. His practice is equipped with state of the art E4D
+                  technology, which can mill a custom crown on-site. Often with a single visit, Dr.
+                  Waters can offer procedures which previously required weeks. Same Day Crown: You
+                  may prefer the convenience and health benefits of eliminating multiple trips when
+                  a crowning procedure is necessary.
+                </p>
+              </b-col>
+              <b-col md="12" lg="4" class="d-flex p-0">
+                <b-container>
+                  <g-image src="~/assets/images/_E4D_Console.jpg" class="p-2 w-100" />
                 </b-container>
               </b-col>
             </b-row>
@@ -130,7 +155,6 @@ h1 {
     font-size: 1.5rem;
   }
 }
-
 p {
   margin-bottom: 1.6em;
 }
@@ -142,17 +166,20 @@ p {
   @media (max-width: 1024px) {
     top: 0;
   }
-
   p {
     font-size: 100%;
   }
 }
-
+.clinically-proven {
+  background-color: #f3f9f9;
+  border-top: 1px solid #e4edec;
+  padding-top: 6rem;
+  // min-height: 1440px;
+}
 .container-fluid {
   background-color: var(--primary);
   max-width: 1210px;
 }
-
 .svg-icon {
   color: #f8f9fa;
   width: 48px;
